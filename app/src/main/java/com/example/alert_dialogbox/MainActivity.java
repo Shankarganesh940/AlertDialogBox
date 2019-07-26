1bfef5555558bc9a0c92a6569f6039401f1c220b
shankarganesh940 <shankarganesh940@gmail.com>
2019-07-26 15:56:07 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/alert_dialogbox/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.alert_dialogbox;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("Title")
+                .setMessage("Example Message")
+                .setPositiveButton("Ok", null)
+                .setNegativeButton("Cancel", null)
+                .show();
+
+        Button positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+        positiveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Not closing", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+    }
+}
